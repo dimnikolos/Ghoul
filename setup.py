66ea8b810152aa8f7762ab7f71e706e440e60c39
Dimitris N <dnikolos@gmail.com>
2014-06-12 15:26:38 +0300

    setup.py for distutils

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 from distutils.core import setup
-import py2exe, sys, os
-
-setup(windows=[{'script': 'Ghoul.py'}],
-    options={
-        'py2exe':
-        {
-            'bundle_files':2
-        }
-    },
-	zipfile = None
-)
+setup(name='Ghoul',
+      version='1.0',
+      description='Scratch Projects Analyzer',
+      author='Dimitris Nikolos',
+      author_email='dnikolos@gmail.com',
+      url='http://dimnikolos.github.io/Ghoul',
+      packages=['Ghoul'],
+     )
